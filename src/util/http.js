8572598466a8e2ffef6d615cc8bbbc9cc80e696a
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-13 11:50:09 +0700

    The Query configuration object and aborting requests

--- src/util/http.js
+++ src/util/http.js
@@ -1,11 +1,15 @@
-export async function fetchEvents(searchTerm) {
-  console.log(searchTerm);
+export async function fetchEvents({ signal, searchTerm }) {
+  console.log("searchTerm", searchTerm);
+  console.log("signal", signal);
+
   let url = "http://localhost:3000/events";
 
   if (searchTerm) {
     url += "?search=" + searchTerm;
   }
-  const response = await fetch(url);
+
+  //signal jika request ingin dibatalkan
+  const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
     const error = new Error("An error occurred while fetching the events");
