86285edc9638e94c4f776103d386183a475b8264
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-14 23:31:35 +0700

    React Query and React Router

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,49 +1,63 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useNavigation,
+  useParams,
+  useSubmit,
+} from "react-router-dom";
 
-import Modal from "../UI/Modal.jsx";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
-import EventForm from "./EventForm.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  //state useNavigation untuk mengetahui status route yang active apakah loading atau bagaimana
+  const { state } = useNavigation();
+  const submit = useSubmit();
   const params = useParams();
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+    staleTime: 10000,
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    //onMutate akan di panggil ketika mutate()function di jalankan
-    //params data ini otomatis ada karena di function handle submit menjalankan mutate dengan object params id dan event
-    onMutate: async (data) => {
-      const newEvent = data.event;
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   //onMutate akan di panggil ketika mutate()function di jalankan
+  //   //params data ini otomatis ada karena di function handle submit menjalankan mutate dengan object params id dan event
+  //   onMutate: async (data) => {
+  //     const newEvent = data.event;
 
-      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
-      //state lama , untuk keperluan jika mutate gagal data di kembalikan ke data sebelumnya
-      const previousEvent = queryClient.getQueryData(["events", params.id]);
-      queryClient.setQueriesData(["events", params.id], newEvent);
+  //     await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+  //     //state lama , untuk keperluan jika mutate gagal data di kembalikan ke data sebelumnya
+  //     const previousEvent = queryClient.getQueryData(["events", params.id]);
+  //     queryClient.setQueriesData(["events", params.id], newEvent);
 
-      //previousEvent ini akan menjadi context di params onError
-      return { previousEvent };
-    },
-    //ketika mutation gagal
-    onError: (error, data, context) => {
-      console.log("masuk on error");
-      queryClient.setQueryData(["events", params.id], context.previousEvent);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["events", params.id]);
-    },
-  });
+  //     //previousEvent ini akan menjadi context di params onError
+  //     return { previousEvent };
+  //   },
+  //   //ketika mutation gagal
+  //   onError: (error, data, context) => {
+  //     console.log("masuk on error");
+  //     queryClient.setQueryData(["events", params.id], context.previousEvent);
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(["events", params.id]);
+  //   },
+  // });
 
   function handleSubmit(formData) {
     //params ini bisa digunakan contohnya di function onMutate diatas
-    mutate({ id: params.id, event: formData });
+    // mutate({ id: params.id, event: formData });
+
+    //cara kedua , submit function ini akan memicu function action di bawah
+    submit(formData, { method: "PUT" });
+    console.log("state", state);
     navigate("../");
   }
 
@@ -52,14 +66,6 @@ export default function EditEvent() {
   }
 
   let content;
-
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
 
   if (isError) {
     content = (
@@ -83,15 +89,39 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            {" "}
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+  //fetch data menggunakan fetcQuery
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  //fetch semua yang key queris nya events
+  await queryClient.invalidateQueries(["events"]);
+  return redirect("../");
 }
