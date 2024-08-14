a9fc64d58ca931f778c54bbb5792335cce9bc7b3
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-14 22:46:37 +0700

    Using the query key as query function input

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -6,8 +6,9 @@ import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["events"],
-    queryFn: fetchEvents,
+    queryKey: ["events", { max: 3 }],
+    //queryKey[1] = {max:3} , untuk mendapatkan array queryKey ke 1
+    queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
     staleTime: 5000, //Untuk menunda request selanjutnya misalkan di tunda selama 5 detik
     // gcTime: 1000, //Berapa lama data akan di keep setelah waktu tsb tercapat data akan di discard
   });
